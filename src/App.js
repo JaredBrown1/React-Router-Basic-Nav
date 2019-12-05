478ad3a2280ff2f67ffbff0eb3fcc77374d90e92
Jared Brown <jared@Jareds-MBP.lan1>
2019-12-04 18:50:45 -0800

    project finished

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => (
   <div>
     <Navigation />
     <nav>
-      <Route exact path="/Home" component={Home} />
+      <Route exact path="/" component={Home} />
       <Route exact path="/About" component={About} />
       <Route exact path="/Contact" component={Contact} />
     </nav>
